3055d27e225b41e0faaa3411e81fc5ceff7d5e92
blue-devil <bluedevil@sctzine.com>
2023-06-07 23:48:02 +0300

    added:writeconsole api example

---
+++ MSYS2/MINGW64/Projects/WinAPI-WriteConsole/WinAPI-WriteConsole.c
@@ -0,0 +1,14 @@
+#include <windows.h>
+
+int main(void)
+{
+    const char * msg = "[*] Pentagram - Lions in cage";
+    HANDLE stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (stdOut != NULL && stdOut != INVALID_HANDLE_VALUE)
+    {
+        DWORD written = 0;
+        WriteConsoleA(stdOut, msg, strlen(msg), &written, NULL);
+    }
+    
+    return 0;
+}
